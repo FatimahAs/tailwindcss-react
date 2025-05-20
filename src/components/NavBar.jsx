e6049afb5ddab287ff437acb6afbd2ca6f638cfb
FATIMAH ASIRI <fatimahasiri@FATIMAHs-MacBook-Pro-2.local>
2025-05-20 13:27:26 +0300

    edit Nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { FaBars } from "react-icons/fa6";
 
 const NavBar = () => {
   return (
 	  <div className='bg-[#fec406] p-2'>
-		  <ul className='flex flex-row  justify-center  align-center gap-4 text-sm'>
+		  <ul className='hidden  md:flex flex-row  justify-center  align-center gap-4 text-sm    '>
 			  <li>All Categories</li>
 			  <li>Today Deals</li>
 			  <li>Electronics</li>
@@ -13,8 +14,10 @@ const NavBar = () => {
 			  <li>Mom & Baby</li>
 			  <li>Books & More</li>
 		  </ul>
+		   <FaBars className='md:hidden text-2xl text-[#333] cursor-pointer' />
 	</div>
   )
 }
 
 export default NavBar
+
